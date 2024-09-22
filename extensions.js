62a3b10f82dcf34770c74c7205ab400957efdaa1
HiHuman-io <team@hihuman.io>
2024-09-22 18:54:29 +0100

    Update extensions.js

--- extensions.js
+++ extensions.js
@@ -562,7 +562,7 @@ export const FeedbackExtension = {
 // Language Detection Module
 export const LanguageDetectionExtension = {
   name: 'LanguageDetection',
-  type: 'end',
+  type: 'effect',
   match: ({ trace }) =>
     trace.type === 'ext_language' || trace.payload.name === 'ext_language',
   render: ({ trace, element }) => {
